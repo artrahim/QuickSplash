a5768556ebbda42ce69224de1a56326ab581d65d
Ahmed <aaa26770123@gmail.com>
2019-04-08 15:29:57 -0600

    Fixed naming

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3003;
+var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '/public')));
 
 var rooms = [];
