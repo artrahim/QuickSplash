19a6788749a220eedb139307b3dcc6454a7fae8f
Masroor Hussain Syed <suroor.23@gmail.com>
2019-04-09 15:10:42 -0600

    getting question asyn 90%

--- questionDb.js
+++ questionDb.js
@@ -8,60 +8,18 @@ let qpDB = mongoose.connection;
 // get Question Model
 let Questions = require('./questionModel');
 
-// let lineReader = require('readline').createInterface({
-//     input: require('fs').createReadStream("./src/Assets/questions/prompts.txt")
-// });
-//
-// lineReader.on('line', function (line) {
-//     console.log(line);
-//     // create and add new account to db
-//     let newQuestion = new Questions({
-//         question: line
-//     });
-//
-//     newQuestion.save(function (err) {
-//         if (err) return "You Fucked up!";
-//     });
-// });
-
-let questions = [];
-console.log(questions);
-
-Questions.find(function (err,doc) {
-    console.log(doc[0].question);
+let lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream("./src/Assets/questions/prompts.txt")
 });
 
+lineReader.on('line', function (line) {
+    console.log(line);
+    // create and add new account to db
+    let newQuestion = new Questions({
+        question: line
+    });
 
-// let question =[];
-// lineReader.on('line', function (line) {
-//     console.log(line);
-//     question.push(line);
-// });
-
-// Questions.countDocuments({}, function(err, count){
-//     console.log(count);
-//     // create and add new account to db
-//     let newQuestion = new Questions({
-//         question: question[],
-//         id: id
-//     });
-//
-//     newQuestion.save(function (err) {
-//         if (err) return "You Fucked up!";
-//     });
-//
-// });
-
-
-// // let id = 0;
-// lineReader.on('line', function (line) {
-//     console.log(line);
-//     // create and add new account to db
-//     let newQuestion = new Questions({
-//         question: line
-//     });
-//
-//     newQuestion.save(function (err) {
-//         if (err) return "You Fucked up!";
-//     });
-// });
+    newQuestion.save(function (err) {
+        if (err) return "You Fucked up!";
+    });
+});
