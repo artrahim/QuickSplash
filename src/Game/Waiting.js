0e6c1507556865ee87f22da7a2b7ba09a9ae3ff2
Ahmed <aaa26770123@gmail.com>
2019-04-12 17:23:53 -0600

    no message

--- src/Game/Waiting.js
+++ src/Game/Waiting.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {socket} from '../Router';
 import $ from 'jquery';
 
@@ -7,6 +7,12 @@ import Logo from "../Game/Utilities/Logo";
 import ButtonSplash from "./Utilities/ButtonSplash";
 import {AllPlayers} from "../Setup/AllPlayers";
 
+import posed from 'react-pose';
+
+import Test from '../Setup/Test'
+
+import {tween, easing, styler, composite, physics} from 'popmotion';
+
 class Waiting extends Component {
 
     constructor(props) {
@@ -14,37 +20,23 @@ class Waiting extends Component {
 
         this.state = {
             allPlayers: [],
-            //splash: props.color,
-            //player: props.player,
         };
     }
 
-
-    componentDidMount(){
+    componentDidMount() {
 
         const lobbyCode = this.props.lobbyCode;
 
-        $('#button').click(function(){
+        $('#button').click(function () {
             socket.emit('startGame', lobbyCode);
         });
 
         socket.on('addPlayers', (players) => {
 
-            // let thisPlayer = this.props.nickname;
-            // let index = 0
-            // for (let i=0; i<players.length; i++){
-            //     if (players[i].nickname === thisPlayer){
-            //         index = i;
-            //     }
-            // };
-            // players.splice(index, 1);
-
             this.setState({
-                allPlayers: players,
+                allPlayers: players
             })
         })
-
-
     }
 
     render() {
@@ -52,14 +44,14 @@ class Waiting extends Component {
         let text = null;
         const isCreator = this.props.isCreator;
         const hasStarted = this.props.hasStarted;
-        if (!hasStarted){
-            if (isCreator){
+        if (!hasStarted) {
+            if (isCreator) {
                 // button = <img id="button" src={ require('../Assets/images/blueSplash.png') } alt="button" />
-                button = <div id = "button"><ButtonSplash imagesource={require('../Assets/images/blueSplash.png')} text={"Start"}/></div>
+                button = <div id="button"><ButtonSplash imagesource={require('../Assets/images/blueSplash.png')}
+                                                        text={"Start"}/></div>
             }
             text = "THE GAME TO START"
-        }
-        else {
+        } else {
             text = "EVERYONE TO ANSWER"
         }
 
@@ -67,7 +59,7 @@ class Waiting extends Component {
             <div className="game">
                 <title>Create a lobby</title>
                 <Logo/>
-                <br></br>
+                <br/>
                 <h1>WAITING FOR {text}...</h1>
                 {button}
                 <AllPlayers allPlayers={this.state.allPlayers}/>
