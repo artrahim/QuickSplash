8d57a3030d0e8d7f18223b88eed30ce58f9294f9
Masroor Hussain Syed <suroor.23@gmail.com>
2019-04-14 21:57:05 -0600

    no msg

--- src/Router.js
+++ src/Router.js
@@ -70,9 +70,9 @@ class Router extends Component {
 
     constructor() {
         super();
-        // this.state = {endpoint: "http://localhost:5000/"};
-        // socket = socketIOClient(this.state.endpoint);
-        socket = io.connect('http://quicksplash.herokuapp.com/');
+        this.state = {endpoint: "http://localhost:5000/"};
+        socket = socketIOClient(this.state.endpoint);
+        // socket = io.connect('http://quicksplash.herokuapp.com/');
         //http.listen(process.env.PORT || 5000);
         //socket = io.connect('http://quicksplash.herokuapp.com/');
     }
