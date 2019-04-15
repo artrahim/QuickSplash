b645ee8a514e8bfb9b899023932c44ff43c75e6b
Artemiss Rahim <artemiss.rahim@ucalgary.ca>
2019-04-14 19:55:00 -0600

    old server

--- src/Router.js
+++ src/Router.js
@@ -70,11 +70,11 @@ class Router extends Component {
 
     constructor() {
         super();
-        // this.state = {endpoint: "http://localhost:5000/"};
-        // socket = socketIOClient(this.state.endpoint);
+        this.state = {endpoint: "http://localhost:5000/"};
+        socket = socketIOClient(this.state.endpoint);
         // socket = io.connect();
         //http.listen(process.env.PORT || 5000);
-        socket = io.connect('http://quicksplash.herokuapp.com/');
+        // socket = io.connect('http://quicksplash.herokuapp.com/');
 
     }
 
